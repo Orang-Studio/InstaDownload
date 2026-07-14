995d8b43fb25523f4c7aa8e1202e6004e8e9c7cc
Vakarux12 <staneviciusvakaris@gmail.com>
2026-07-14 10:57:31 +0300

    Bump to 2.3.4 and show app version above credit

--- InstaDownload/app/build.gradle.kts
+++ InstaDownload/app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         minSdk = 24  // Android 7.0 (Nougat)
         //noinspection OldTargetApi
         targetSdk = 35
-        versionCode = 10
-        versionName = "2.3.3"
+        versionCode = 11
+        versionName = "2.3.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
