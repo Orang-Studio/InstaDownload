ebd6283997f08a35b092b6cebfc256f02b6b89f1
Vakarux12 <staneviciusvakaris@gmail.com>
2026-08-03 11:23:06 +0300

    Bump to 2.5.0

--- InstaDownload/app/build.gradle.kts
+++ InstaDownload/app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         minSdk = 24  // Android 7.0 (Nougat)
         //noinspection OldTargetApi
         targetSdk = 35
-        versionCode = 12
-        versionName = "2.4.0"
+        versionCode = 13
+        versionName = "2.5.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
