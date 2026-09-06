508b18bfb1f09ad4d54d9db2fe67c9dfd0863fa7
Vakarux12 <staneviciusvakaris@gmail.com>
2026-09-07 00:20:51 +0300

    Bump to 2.6.0-stories

--- InstaDownload/app/build.gradle.kts
+++ InstaDownload/app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         minSdk = 24  // Android 7.0 (Nougat)
         //noinspection OldTargetApi
         targetSdk = 35
-        versionCode = 13
-        versionName = "2.5.0-stories"
+        versionCode = 14
+        versionName = "2.6.0-stories"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
