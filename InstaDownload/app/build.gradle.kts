418e3ec0c645282ae822c3676f995f826663180a
Vakarux12 <staneviciusvakaris@gmail.com>
2026-09-20 19:48:02 +0300

    Bump to 2.7.1

--- InstaDownload/app/build.gradle.kts
+++ InstaDownload/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.vakarux.instadownload"
         minSdk = 24  // Android 7.0 (Nougat)
         targetSdk = 36
-        versionCode = 16
-        versionName = "2.7.0"
+        versionCode = 17
+        versionName = "2.7.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
